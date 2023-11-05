e0a8ede661c4978bce1e1da3538d1555bc1a5494
David Hernandez <davidfelipehernandez1@gmail.com>
2023-11-05 11:44:30 -0500

    min

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const Center(
-        child: Text('miguelmor'),
+        child: Text('miguelmor no seas asi'),
       ),
     );
   }
