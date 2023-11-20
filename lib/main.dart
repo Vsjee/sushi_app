42c69edac44b9d0b27e446593a330f895fcc1325
David Hernandez <davidfelipehernandez1@gmail.com>
2023-11-19 22:17:32 -0500

    add initialization for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sushi_app/firebase_options.dart';
 import 'package:sushi_app/intro_page.dart';
 import 'package:sushi_app/login_page.dart';
 import 'package:sushi_app/modules/private/home/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     const ProviderScope(
       child: MyApp(),
